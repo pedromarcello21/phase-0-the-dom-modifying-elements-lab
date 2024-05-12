a4c800a5de65bf9a6ed700fe03e4d2aa7c19d357
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-05-12 15:33:08 -0400

    dom practice

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+document.getElementById("main").remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.innerHTML = "Pedro is the champion"
+
+newHeader.id = "victory"
+
+newHeader.style.color = "blue"
+
+document.body.append(newHeader);
+
+
